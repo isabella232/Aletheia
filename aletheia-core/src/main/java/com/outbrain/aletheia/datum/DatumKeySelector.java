0452db733b7762a9eaececd72c4e275d48c477c3
staslev <stas.levin@gmail.com>
2015-06-04 11:26:53 +0300

    Added json type info annotation for DatumKeySelector.

--- aletheia-core/src/main/java/com/outbrain/aletheia/datum/DatumKeySelector.java
+++ aletheia-core/src/main/java/com/outbrain/aletheia/datum/DatumKeySelector.java
@@ -1,8 +1,11 @@
 package com.outbrain.aletheia.datum;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 /**
  * Defines the interface to be implemented by the various datum key selection strategies.
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public interface DatumKeySelector<TDomainClass> {
 
   DatumKeySelector NULL = new DatumKeySelector() {
