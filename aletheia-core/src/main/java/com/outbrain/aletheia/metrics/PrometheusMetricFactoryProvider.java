3f5fcdc7780d3207c523bf98deaedaffb2b55b60
averman <averman@outbrain.com>
2018-07-24 14:53:55 +0300

    fix labels

--- aletheia-core/src/main/java/com/outbrain/aletheia/metrics/PrometheusMetricFactoryProvider.java
+++ aletheia-core/src/main/java/com/outbrain/aletheia/metrics/PrometheusMetricFactoryProvider.java
@@ -15,7 +15,7 @@ public class PrometheusMetricFactoryProvider extends AletheiaMetricFactoryProvid
 
   @Override
   public MetricsFactory forAuditingDatumProducer(final EndPoint endPoint) {
-    return buildMetricsFactory(endPoint);
+    return getMetricsFactory(endPoint, PRODUCTION);
   }
 
   @Override
@@ -32,34 +32,34 @@ public class PrometheusMetricFactoryProvider extends AletheiaMetricFactoryProvid
   @Override
   public MetricsFactory forDatumEnvelopeSender(final EndPoint endPoint) {
 
-    return buildMetricsFactory(endPoint);
+    return getMetricsFactory(endPoint, PRODUCTION);
 
   }
 
   @Override
   public MetricsFactory forDatumEnvelopeFetcher(final EndPoint endPoint) {
-    return buildMetricsFactory(endPoint);
-
+    return getMetricsFactory(endPoint, CONSUMPTION);
   }
 
   @Override
   public MetricsFactory forAuditingDatumStreamConsumer(final EndPoint endPoint) {
-    return buildMetricsFactory(endPoint);
+    return getMetricsFactory(endPoint, CONSUMPTION);
 
   }
 
   @Override
   public MetricsFactory forDatumEnvelopeMeta(final EndPoint endPoint) {
-    return buildMetricsFactory(endPoint);
+    return getMetricsFactory(endPoint, CONSUMPTION);
 
   }
 
-  private MetricsFactory buildMetricsFactory(EndPoint endPoint) {
+
+  private MetricsFactory getMetricsFactory(EndPoint endPoint, String consumption) {
     Map<String, String> labels = new HashMap<>();
     labels.put(DATUM_TYPE_ID, datumTypeId());
     labels.put(COMPONENT, componentName);
     labels.put(ENDPOINT_NAME, endPoint.getName());
-    labels.put(DIRECTION, PRODUCTION);
+    labels.put(DIRECTION, consumption);
     labels.put(ENDPOINT_CLASS, endPoint.getClass().getSimpleName());
 
 
